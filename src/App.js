cb18be4fe44267cb64c211bd472f425a51a0577b
gsigor84 <gsigor84@gmail.com>
2023-12-05 20:48:05 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useRef } from 'react'
 import emailjs from '@emailjs/browser';
 import './App.css'
 
@@ -23,88 +23,97 @@ function App() {
 
   return (
     <div className="App">
-      <div className="parent">
-        <div className="child">
 
-        </div>
-      </div>
       <div className="main">
+ 
 
         <form ref={form} onSubmit={sendEmail} >
           <p className="title"> Team Randy Form</p>
+          <div class="row">
+            <p>Full Name:
+              <input type="text"
 
-          <p>Full Name:
-            <input type="text"
+                name="name"
+              /></p>
+          </div>
 
-              name="name"
-            /></p>
-          <p>Age:
-            <input type="text"
+          <div class="row">
+            <p>Age:
+              <input type="text"
 
-              name="age"
-            /></p>
-          <p>Email Address:
-            <input type="text"
-              name="email"
-            /></p>
-          <p>Phone Number for WhatsApp Contact:
-            <input type="text"
-              name="phone"
-            /></p>
+                name="age"
+              /></p>
+          </div>
+          <div class="row">
+            <p>Email Address:
+              <input type="text"
+                name="email"
+              /></p></div>
+          <div class="row">
+            <p>Phone Number Contact:
+              <input type="text"
+                name="phone"
+              /></p>
+          </div>
 
-          <p>Which services are you interested in?</p>
-
-
-          <input type="checkbox"
-
-          />
-          <label > Nutrition + Training</label><br></br>
-
-          <input type="checkbox"
-
-            name="nutritio+training"
-          />
-          <label > Training</label><br />
-
-          <input type="checkbox"
-
-            name="nutrition"
-          />
-          <label > Nutrition</label><br />
-
-          <input type="checkbox"
-
-            name="randyExperience"
-          />
-          <label > Randy Experience (Training together + discussion)</label><br />
-          <p>Other: <input type="text"
-
-            name="other"
-          /></p>
+          <div class="row">
+            <p>Which services are you interested in?</p>
 
 
-          <p>Have you followed a coaching plan before?</p>
-          <p>
             <input type="checkbox"
 
-              name="yesCoaching" />
-            <label > Yes</label> </p>
-          <p>
-            <input type="checkbox"
-
-              name="noCoaching"
             />
-            <label > No</label></p>
+            <label > Nutrition + Training</label><br></br>
+
+            <input type="checkbox"
+
+              name="nutritio+training"
+            />
+            <label > Training</label><br />
+
+            <input type="checkbox"
+
+              name="nutrition"
+            />
+            <label > Nutrition</label><br />
+
+            <input type="checkbox"
+
+              name="randyExperience"
+            />
+            <label > Randy Experience (Training together + discussion)</label><br />
+            <p>Other: <input type="text"
+
+              name="other"
+            /></p>
+          </div>
+          <div class="row">
 
 
-          <p>What is your goal?</p>
-          <input type="text"
-            name="goal"
-          />
-          <p>Why are you choosing me?</p>
-          <input type="text"
-            name="why"
-          />
+            <p>Have you followed a coaching plan before?</p>
+            <p>
+              <input type="checkbox"
+
+                name="yesCoaching" />
+              <label > Yes</label> </p>
+            <p>
+              <input type="checkbox"
+
+                name="noCoaching"
+              />
+              <label > No</label></p>
+          </div>
+          <div class="row">
+
+            <p>What is your goal?</p>
+            <input type="text"
+              name="goal"
+            />
+            <p>Why are you choosing me?</p>
+            <input type="text"
+              name="why"
+            />
+          </div>
           <p><button type="submit">Click Me!</button></p>
         </form>
 
